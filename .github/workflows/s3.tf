e89e1275bc2d1365574a86a5473518313cfd47bb
Prakash Jadhav <prakashsjadhav2012@gmail.com>
2022-06-18 21:14:06 +0530

    Recent changes in all the files

---
+++ .github/workflows/s3.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "inance-psj-18062022"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
