74ba810b910ec51338ecd5a4927f6ad416d0b2c9
Prakash Jadhav <prakashsjadhav2012@gmail.com>
2022-07-01 14:59:13 +0530

    Again added provider to S3 website

--- s3-website.tf
+++ s3-website.tf
@@ -3,7 +3,9 @@
 #------------------------------------------------------------------------------
 #tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-logging
 
-
+provider "aws" {
+  region = "eu-west-2"
+}
 
 locals {
 
