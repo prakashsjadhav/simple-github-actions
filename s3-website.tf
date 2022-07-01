534c880cea4f98714e440bc5a7143cbf9270671f
Prakash Jadhav <prakashsjadhav2012@gmail.com>
2022-07-01 23:28:52 +0530

    path.module

--- s3-website.tf
+++ s3-website.tf
@@ -59,7 +59,7 @@ resource "aws_s3_bucket_website_configuration" "website" {
 
 resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.website.id
-  policy = templatefile("/templates/website_bucket_policy.json", {
+  policy = templatefile("${path.module}/templates/website_bucket_policy.json", {
   })
 }
 
