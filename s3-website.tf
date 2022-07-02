c0abc6659529f48dc2f2358c94c6c00df647d4a0
Prakash Jadhav <prakashsjadhav2012@gmail.com>
2022-07-02 13:58:04 +0530

    changed policy10

--- s3-website.tf
+++ s3-website.tf
@@ -52,7 +52,7 @@ resource "aws_s3_bucket_website_configuration" "website" {
 
 resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.website.id
-  policy = templatefile("${path.module}/template/s3_website_bucket_policy.json", {
+  policy = templatefile("s3_website_bucket_policy.json", {
   bucket_name = local.website_bucket_name
   })
 }
